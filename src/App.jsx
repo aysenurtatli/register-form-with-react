8481140e0bcc6bb613d30a342c86b0dea9359589
Aysenur Tatli <aysenurtatlii@hotmail.com>
2024-12-20 18:25:17 +0300

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './parts/Header'
+import Footer from './parts/Footer'
 import { Form } from './Form'
 function App() {
   return (
     <>
       <Header></Header>
       <Form></Form>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/parts/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return(
+        <div>
+            <footer className="container flex justify-center items-center">
+                <p className="text-indigo-300 font-light">©2024 Aysenur Tatli</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
